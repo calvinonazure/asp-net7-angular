410efd15474a11fb212c0b722ed98a054b9d5de8
Calvin Cai <yuncai@microsoft.com>
2023-12-20 14:48:06 +0800

    first commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using System.Net.Mail;
+
+namespace MyFirstAzureWebApp.Controllers
+{
+    public class TestController : Controller
+    {
+        [Route("api/test")]
+        public IActionResult Index()
+        {
+            var result = new
+            {
+                comments = "Some string",
+                attachments = "Som attachementes"
+            };
+
+            return this.Json(result);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# asp.net net7 with angular
+
+A sample project contains asp.net combine with angular distributed files deploy to Azure App Service. 
+
+Refer web.config for the URL rewriting details
+
+
+
